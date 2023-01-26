13ffe77e508404c2a8280dfccf0c5b7e43e51872
Jean Marcel <jeanvanzella91@gmail.com>
2023-01-25 23:59:45 -0300

    update sutudent bio

---
+++ precache-manifest.b2e4962863476adf24c8e1186119fb16.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ba8b18d40c2306fe81f6fe98710443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "828e5e5a4c3c56b8539e",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "04b0731d3d2827e25741",
+    "url": "/static/css/main.52226492.chunk.css"
+  },
+  {
+    "revision": "828e5e5a4c3c56b8539e",
+    "url": "/static/js/2.2012ff58.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2012ff58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b0731d3d2827e25741",
+    "url": "/static/js/main.135d7e74.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "818576fe4c891e295d03aa336a3e6ad3",
+    "url": "/static/media/about.818576fe.jpg"
+  },
+  {
+    "revision": "a06776c617a21ab59815f80298e8a66b",
+    "url": "/static/media/background.a06776c6.jpg"
+  },
+  {
+    "revision": "d727f23f70c8e5061469f1b5be2859cd",
+    "url": "/static/media/brasilIcon.d727f23f.png"
+  },
+  {
+    "revision": "02967b3517c3b2f1801389e8b8d10467",
+    "url": "/static/media/css.02967b35.png"
+  },
+  {
+    "revision": "44e3cbf4a7632bfe6ea94eb7fba9da91",
+    "url": "/static/media/html.44e3cbf4.png"
+  },
+  {
+    "revision": "7e03e886bf001a3bbf8f6d45111af133",
+    "url": "/static/media/mongoDB.7e03e886.png"
+  },
+  {
+    "revision": "caacdb3470282a51e0bca29fab0d454e",
+    "url": "/static/media/projeto-1.caacdb34.png"
+  },
+  {
+    "revision": "caacdb3470282a51e0bca29fab0d454e",
+    "url": "/static/media/projeto-2.caacdb34.png"
+  },
+  {
+    "revision": "caacdb3470282a51e0bca29fab0d454e",
+    "url": "/static/media/projeto-3.caacdb34.png"
+  },
+  {
+    "revision": "0491aa306d3a7d30fb3ee3fc5a58eb0b",
+    "url": "/static/media/python.0491aa30.png"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/static/media/react.22808bae.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "bc505dd37ef7799e1ad269bcc0be9245",
+    "url": "/static/media/typescript.bc505dd3.png"
+  },
+  {
+    "revision": "e2fd4f21fdb9fdfa9ca9292bc0ad7b59",
+    "url": "/static/media/usaIcon.e2fd4f21.png"
+  }
+]);
